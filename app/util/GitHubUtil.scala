37f6cd61b6153536c4dee874611cf68c6e888377
Jadon Fowler <jadonflower@gmail.com>
2018-03-13 23:09:29 -0700

    Support any GitHub README when scraping
    GitHub has a lovely API for retrieving a repository's README.
    
    Signed-off-by: Jadon Fowler <jadonflower@gmail.com>

--- app/util/GitHubUtil.scala
+++ app/util/GitHubUtil.scala
@@ -2,19 +2,23 @@ package util
 
 import java.io.FileNotFoundException
 
+import play.api.libs.json.{JsValue, Json}
+
 import scala.io.Source
 
 object GitHubUtil {
 
   private val identifier = "A-Za-z0-9-_"
   private val gitHubUrlPattern = s"""http(s)?://github.com/[$identifier]+/[$identifier]+(/)?""".r.pattern
-  private val readmeUrl = "https://raw.githubusercontent.com/%s/%s/master/README.md"
+  private val readmeApi = "https://api.github.com/repos/%s/%s/readme"
 
   def isGitHubUrl(url: String): Boolean = gitHubUrlPattern.matcher(url).matches()
 
   def getReadme(user: String, project: String): Option[String] = {
     try {
-      val readme = Source.fromURL(readmeUrl.format(user, project)).mkString
+      val readmeApiJson: JsValue = Json.parse(Source.fromURL(readmeApi.format(user, project)).mkString)
+      val url = (readmeApiJson \ "download_url").get.toString()
+      val readme = Source.fromURL(url).mkString
       Some(readme)
     } catch {
       case _: FileNotFoundException => None
