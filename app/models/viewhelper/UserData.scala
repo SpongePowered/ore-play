81dd16d5a47d4d3cdfb291c858d5cd817ff7cb15
Anselm Brehme <development.anselm@breh.me>
2018-03-24 14:51:51 +0100

    fix user organizations not showing up

--- app/models/viewhelper/UserData.scala
+++ app/models/viewhelper/UserData.scala
@@ -3,12 +3,14 @@ package models.viewhelper
 import controllers.routes
 import controllers.sugar.Requests.OreRequest
 import db.ModelService
+import db.impl.{OrganizationRoleTable, OrganizationTable, UserTable}
 import models.user.role.OrganizationRole
 import models.user.{Organization, User}
 import ore.permission._
 import play.api.cache.AsyncCacheApi
 import slick.jdbc.JdbcBackend
-
+import slick.lifted.TableQuery
+import db.impl.OrePostgresDriver.api._
 import scala.concurrent.{ExecutionContext, Future}
 
 // TODO separate Scoped UserData
@@ -17,7 +19,7 @@ case class UserData(headerData: HeaderData,
                     user: User,
                     isOrga: Boolean,
                     projectCount: Int,
-                    orgas: Seq[(Organization, OrganizationRole, User)],
+                    orgas: Seq[(Organization, User, OrganizationRole, User)],
                     userPerm: Map[Permission, Boolean],
                     orgaPerm: Map[Permission, Boolean]) {
 
@@ -45,6 +47,23 @@ object UserData {
   val noUserPerms: Map[Permission, Boolean] = Map(ViewActivity -> false, ReviewFlags -> false, ReviewProjects -> false)
   val noOrgaPerms: Map[Permission, Boolean] = Map(EditSettings -> false)
 
+  private def queryRoles(user: User) = {
+    val roleTable = TableQuery[OrganizationRoleTable]
+    val orgaTable = TableQuery[OrganizationTable]
+    val userTable = TableQuery[UserTable]
+
+    for {
+      r <- roleTable if r.userId === user.id.get
+      o <- orgaTable if r.organizationId === o.id
+      u <- userTable if o.id === u.id
+      uo <- userTable if o.userId === uo.id
+    } yield {
+      (o, u, r, uo) // Organization OrgaUser Role Owner
+    }
+
+  }
+
+
   def of[A](request: OreRequest[A], user: User)(implicit cache: AsyncCacheApi, db: JdbcBackend#DatabaseDef, ec: ExecutionContext, service: ModelService): Future[UserData] = {
     for {
       isOrga <- user.isOrganization
@@ -54,6 +73,7 @@ object UserData {
       reviewFlags <- user can ReviewFlags in user map ((ReviewFlags, _))
       reviewProjects <- user can ReviewProjects in user map ((ReviewProjects, _))
       editSettings <- user can EditSettings in orga map ((EditSettings, _))
+      orgas <- db.run(queryRoles(user).result)
     } yield {
 
       val userPerms: Map[Permission, Boolean] = Seq(viewActivity, reviewFlags, reviewProjects).toMap
@@ -63,7 +83,7 @@ object UserData {
               user,
               isOrga,
               projectCount,
-              Seq.empty,
+              orgas,
               userPerms,
               orgaPerms)
 
