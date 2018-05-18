0d66b0b1872449667fd3232c90b5c65375405e91
Unknown <katrix97@hotmail.com>
2018-05-18 21:20:07 +0200

    Make Ore localizable

---
+++ conf/evolutions/default/95.sql
@@ -0,0 +1,17 @@
+# --- !Ups
+
+ALTER TABLE users ADD COLUMN language VARCHAR(255);
+
+ALTER TABLE notifications ADD COLUMN messageArgs VARCHAR(255)[];
+UPDATE notifications SET messageArgs = [message];
+
+ALTER TABLE notifications DROP COLUMN message;
+
+# --- !Downs
+
+ALTER TABLE users DROP COLUMN language;
+
+ALTER TABLE notifications ADD COLUMN message VARCHAR(255);
+UPDATE notifications SET message = messageArgs[0];
+
+ALTER TABLE notifications DROP COLUMN messageArgs;
