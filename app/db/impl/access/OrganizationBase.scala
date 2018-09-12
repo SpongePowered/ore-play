RedNesto/Ore

Better case classes and model bases

app/db/impl/access/OrganizationBase.scala
@@ -1,7 +1,6 @@
 package db.impl.access
 
 import db.{ModelBase, ModelService}
-import discourse.OreDiscourseApi
 import models.user.role.OrganizationRole
 import models.user.{Notification, Organization}
 import ore.OreConfig
@@ -16,12 +15,9 @@ import scala.concurrent.{ExecutionContext, Future}
 
 import util.functional.{EitherT, OptionT}
 
-class OrganizationBase(override val service: ModelService,
-                       forums: OreDiscourseApi,
-                       auth: SpongeAuthApi,
+class OrganizationBase(implicit val service: ModelService,
                        config: OreConfig,
-                       messages: MessagesApi,
-                       implicit val users: UserBase)
+                       messages: MessagesApi)
                        extends ModelBase[Organization] {
 
   override val modelClass: Class[Organization] = classOf[Organization]
@@ -36,7 +32,7 @@ class OrganizationBase(override val service: ModelService,
     * @param ownerId  User ID of the organization owner
     * @return         New organization if successful, None otherwise
     */
-  def create(name: String, ownerId: Int, members: Set[OrganizationRole])(implicit cache: AsyncCacheApi, ec: ExecutionContext): EitherT[Future, String, Organization] = {
+  def create(name: String, ownerId: Int, members: Set[OrganizationRole])(implicit cache: AsyncCacheApi, ec: ExecutionContext, auth: SpongeAuthApi): EitherT[Future, String, Organization] = {
     Logger.debug("Creating Organization...")
     Logger.debug("Name     : " + name)
     Logger.debug("Owner ID : " + ownerId)
@@ -48,7 +44,7 @@ class OrganizationBase(override val service: ModelService,
     // By default we use "<org>@ore.spongepowered.org".
     Logger.debug("Creating on SpongeAuth...")
     val dummyEmail = name + '@' + this.config.orgs.get[String]("dummyEmailDomain")
-    val spongeResult = this.auth.createDummyUser(name, dummyEmail, verified = true)
+    val spongeResult = auth.createDummyUser(name, dummyEmail, verified = true)
 
     // Check for error
     spongeResult.leftMap { err =>
@@ -60,7 +56,7 @@ class OrganizationBase(override val service: ModelService,
       // reference to the Sponge user ID, the organization's username and a
       // reference to the User owner of the organization.
       Logger.debug("Creating on Ore...")
-      this.add(Organization(id = Some(spongeUser.id), username = name, _ownerId = ownerId))
+      this.add(Organization(id = Some(spongeUser.id), username = name, ownerId = ownerId))
     }.semiFlatMap { org =>
       // Every organization model has a regular User companion. Organizations
       // are just normal users with additional information. Adding the
@@ -68,13 +64,13 @@ class OrganizationBase(override val service: ModelService,
       // and should be treated as such.
       for {
         userOrg <- org.toUser.getOrElse(throw new IllegalStateException("User not created"))
-        _ = userOrg.setGlobalRoles(userOrg.globalRoles + RoleTypes.Organization)
+        _ <- service.update(userOrg.copy(globalRoles = RoleTypes.Organization :: userOrg.globalRoles))
         _ <- // Add the owner
           org.memberships.addRole(OrganizationRole(
             userId = ownerId,
             organizationId = org.id.get,
-            _roleType = RoleTypes.OrganizationOwner,
-            _isAccepted = true))
+            roleType = RoleTypes.OrganizationOwner,
+            isAccepted = true))
         _ <- {
           // Invite the User members that the owner selected during creation.
           Logger.debug("Inviting members...")
@@ -105,4 +101,9 @@ class OrganizationBase(override val service: ModelService,
     */
   def withName(name: String)(implicit ec: ExecutionContext): OptionT[Future, Organization] = this.find(StringUtils.equalsIgnoreCase(_.name, name))
 
+}
+object OrganizationBase {
+  def apply()(implicit organizationBase: OrganizationBase): OrganizationBase = organizationBase
+
+  implicit def fromService(implicit service: ModelService): OrganizationBase = service.getModelBase(classOf[OrganizationBase])
 }

app/util/DataHelper.scala
@@ -1,34 +1,35 @@
 package util
 
 import com.google.common.base.Preconditions.checkArgument
+
 import db.ModelService
 import db.access.ModelAccess
 import db.impl.access.{ProjectBase, UserBase}
 import discourse.OreDiscourseApi
 import javax.inject.Inject
+
 import models.project.{Channel, Project, ProjectSettings, Version}
 import models.user.User
 import ore.OreConfig
 import ore.project.factory.ProjectFactory
 import play.api.cache.SyncCacheApi
-
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 /**
   * Utility class for performing some bulk actions on the application data.
   * Typically for testing.
   */
-final class DataHelper @Inject()(config: OreConfig,
+final class DataHelper @Inject()(implicit config: OreConfig,
                                  statusZ: StatusZ,
                                  service: ModelService,
                                  factory: ProjectFactory,
                                  forums: OreDiscourseApi,
                                  cacheApi: SyncCacheApi) {
 
-  implicit private val projects: ProjectBase = this.service.getModelBase(classOf[ProjectBase])
+  private val projects = ProjectBase.fromService(service)
   private val channels: ModelAccess[Channel] = this.service.access[Channel](classOf[Channel])
   private val versions: ModelAccess[Version] = this.service.access[Version](classOf[Version])
-  private val users: UserBase = this.service.getModelBase(classOf[UserBase])
+  private val users = UserBase.fromService(service)
 
   val Logger = play.api.Logger("DataHelper")
 
@@ -57,7 +58,7 @@ final class DataHelper @Inject()(config: OreConfig,
     *
     * @param users Amount of users to create
     */
-  def seed(users: Int, projects: Int, versions: Int, channels: Int)(implicit ec: ExecutionContext): Unit = {
+  def seed(users: Int, projects: Int, versions: Int, channels: Int)(implicit ec: ExecutionContext, service: ModelService): Unit = {
     if (sys.env.getOrElse(statusZ.SpongeEnv, "unknown") != "local") return
     // Note: Dangerous as hell, handle with care
     Logger.info("---- Seeding Ore ----")
@@ -74,7 +75,7 @@ final class DataHelper @Inject()(config: OreConfig,
     var projectNum = 0
     for (i <- 0 until users) {
       Logger.info(Math.ceil(i / users.asInstanceOf[Float] * 100).asInstanceOf[Int].toString + "%")
-      this.users.add(User(id = Some(i), _username = s"User-$i")).map { user =>
+      this.users.add(User(id = Some(i), name = s"User-$i")).map { user =>
         // Create some projects
         for (j <- 0 until projects) {
           val pluginId = s"plugin$projectNum"
@@ -84,7 +85,7 @@ final class DataHelper @Inject()(config: OreConfig,
             .ownerId(user.id.get)
             .name(s"Project$projectNum")
             .build()) map { project =>
-            project.updateSettings(this.service.processor.process(ProjectSettings()))
+            project.updateSettings(ProjectSettings())
             // Now create some additional versions for this project
             var versionNum = 0
             for (k <- 0 until channels) {
@@ -96,12 +97,12 @@ final class DataHelper @Inject()(config: OreConfig,
                     channelId = channel.id.get,
                     fileSize = 1,
                     hash = "none",
-                    _authorId = i,
+                    authorId = i,
                     fileName = "none",
                     signatureFileName = "none")) map { version =>
-                    if (l == 0)
-                      project.setRecommendedVersion(version)
                     versionNum += 1
+                    if (l == 0) service.update(project.copy(recommendedVersionId = Some(version.id.get)))
+                    else Future.unit
                   }
                 }
               }

SpongePowered/ore-play

app/db/impl/access/OrganizationBase.scala
@@ -1,7 +1,6 @@
 package db.impl.access
 
 import db.{ModelBase, ModelService, ObjectId}
-import discourse.OreDiscourseApi
 import models.user.role.OrganizationRole
 import models.user.{Notification, Organization}
 import ore.OreConfig
@@ -16,12 +15,9 @@ import scala.concurrent.{ExecutionContext, Future}
 
 import util.functional.{EitherT, OptionT}
 
-class OrganizationBase(override val service: ModelService,
-                       forums: OreDiscourseApi,
-                       auth: SpongeAuthApi,
+class OrganizationBase(implicit val service: ModelService,
                        config: OreConfig,
-                       messages: MessagesApi,
-                       implicit val users: UserBase)
+                       messages: MessagesApi)
                        extends ModelBase[Organization] {
 
   override val modelClass: Class[Organization] = classOf[Organization]
@@ -36,7 +32,7 @@ class OrganizationBase(override val service: ModelService,
     * @param ownerId  User ID of the organization owner
     * @return         New organization if successful, None otherwise
     */
-  def create(name: String, ownerId: Int, members: Set[OrganizationRole])(implicit cache: AsyncCacheApi, ec: ExecutionContext): EitherT[Future, String, Organization] = {
+  def create(name: String, ownerId: Int, members: Set[OrganizationRole])(implicit cache: AsyncCacheApi, ec: ExecutionContext, auth: SpongeAuthApi): EitherT[Future, String, Organization] = {
     Logger.debug("Creating Organization...")
     Logger.debug("Name     : " + name)
     Logger.debug("Owner ID : " + ownerId)
@@ -48,7 +44,7 @@ class OrganizationBase(override val service: ModelService,
     // By default we use "<org>@ore.spongepowered.org".
     Logger.debug("Creating on SpongeAuth...")
     val dummyEmail = name + '@' + this.config.orgs.get[String]("dummyEmailDomain")
-    val spongeResult = this.auth.createDummyUser(name, dummyEmail, verified = true)
+    val spongeResult = auth.createDummyUser(name, dummyEmail, verified = true)
 
     // Check for error
     spongeResult.leftMap { err =>
@@ -60,7 +56,7 @@ class OrganizationBase(override val service: ModelService,
       // reference to the Sponge user ID, the organization's username and a
       // reference to the User owner of the organization.
       Logger.info("Creating on Ore...")
-      this.add(Organization(id = ObjectId(spongeUser.id), username = name, _ownerId = ownerId))
+      this.add(Organization(id = ObjectId(spongeUser.id), username = name, ownerId = ownerId))
     }.semiFlatMap { org =>
       // Every organization model has a regular User companion. Organizations
       // are just normal users with additional information. Adding the
@@ -68,13 +64,13 @@ class OrganizationBase(override val service: ModelService,
       // and should be treated as such.
       for {
         userOrg <- org.toUser.getOrElse(throw new IllegalStateException("User not created"))
-        _ = userOrg.setGlobalRoles(userOrg.globalRoles + RoleType.Organization)
+        _ <- service.update(userOrg.copy(globalRoles = RoleType.Organization :: userOrg.globalRoles))
         _ <- // Add the owner
           org.memberships.addRole(OrganizationRole(
             userId = ownerId,
             organizationId = org.id.value,
-            _roleType = RoleType.OrganizationOwner,
-            _isAccepted = true))
+            roleType = RoleType.OrganizationOwner,
+            isAccepted = true))
         _ <- {
           // Invite the User members that the owner selected during creation.
           Logger.debug("Inviting members...")
@@ -105,4 +101,9 @@ class OrganizationBase(override val service: ModelService,
     */
   def withName(name: String)(implicit ec: ExecutionContext): OptionT[Future, Organization] = this.find(StringUtils.equalsIgnoreCase(_.name, name))
 
+}
+object OrganizationBase {
+  def apply()(implicit organizationBase: OrganizationBase): OrganizationBase = organizationBase
+
+  implicit def fromService(implicit service: ModelService): OrganizationBase = service.getModelBase(classOf[OrganizationBase])
 }

app/util/DataHelper.scala
@@ -13,23 +13,23 @@ import models.user.User
 import ore.OreConfig
 import ore.project.factory.ProjectFactory
 import play.api.cache.SyncCacheApi
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 /**
   * Utility class for performing some bulk actions on the application data.
   * Typically for testing.
   */
-final class DataHelper @Inject()(config: OreConfig,
+final class DataHelper @Inject()(implicit config: OreConfig,
                                  statusZ: StatusZ,
                                  service: ModelService,
                                  factory: ProjectFactory,
                                  forums: OreDiscourseApi,
                                  cacheApi: SyncCacheApi) {
 
-  implicit private val projects: ProjectBase = this.service.getModelBase(classOf[ProjectBase])
+  private val projects = ProjectBase.fromService(service)
   private val channels: ModelAccess[Channel] = this.service.access[Channel](classOf[Channel])
   private val versions: ModelAccess[Version] = this.service.access[Version](classOf[Version])
-  private val users: UserBase = this.service.getModelBase(classOf[UserBase])
+  private val users = UserBase.fromService(service)
 
   val Logger = play.api.Logger("DataHelper")
 
@@ -58,7 +58,7 @@ final class DataHelper @Inject()(config: OreConfig,
     *
     * @param users Amount of users to create
     */
-  def seed(users: Int, projects: Int, versions: Int, channels: Int)(implicit ec: ExecutionContext): Unit = {
+  def seed(users: Int, projects: Int, versions: Int, channels: Int)(implicit ec: ExecutionContext, service: ModelService): Unit = {
     if (sys.env.getOrElse(statusZ.SpongeEnv, "unknown") != "local") return
     // Note: Dangerous as hell, handle with care
     Logger.info("---- Seeding Ore ----")
@@ -75,7 +75,7 @@ final class DataHelper @Inject()(config: OreConfig,
     var projectNum = 0
     for (i <- 0 until users) {
       Logger.info(Math.ceil(i / users.asInstanceOf[Float] * 100).asInstanceOf[Int].toString + "%")
-      this.users.add(User(id = ObjectId(i), _username = s"User-$i")).map { user =>
+      this.users.add(User(id = ObjectId(i), name = s"User-$i")).map { user =>
         // Create some projects
         for (j <- 0 until projects) {
           val pluginId = s"plugin$projectNum"
@@ -85,7 +85,7 @@ final class DataHelper @Inject()(config: OreConfig,
             .ownerId(user.id.value)
             .name(s"Project$projectNum")
             .build()) map { project =>
-            project.updateSettings(this.service.processor.process(ProjectSettings()))
+            project.updateSettings(ProjectSettings())
             // Now create some additional versions for this project
             var versionNum = 0
             for (k <- 0 until channels) {
@@ -94,15 +94,15 @@ final class DataHelper @Inject()(config: OreConfig,
                   this.versions.add(Version(
                     projectId = project.id.value,
                     versionString = versionNum.toString,
-                    _channelId = channel.id.value,
+                    channelId = channel.id.value,
                     fileSize = 1,
                     hash = "none",
-                    _authorId = i,
+                    authorId = i,
                     fileName = "none",
                     signatureFileName = "none")) map { version =>
-                    if (l == 0)
-                      project.setRecommendedVersion(version)
                     versionNum += 1
+                    if (l == 0) service.update(project.copy(recommendedVersionId = Some(version.id.value)))
+                    else Future.unit
                   }
                 }
               }