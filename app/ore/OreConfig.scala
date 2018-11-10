0716b1168cc69888b43507e4bfe8e2c5bd6a8ebe
Katrix <katrix97@hotmail.com>
2018-11-10 19:58:22 +0100

    Don't use lazy for values in config

--- app/ore/OreConfig.scala
+++ app/ore/OreConfig.scala
@@ -22,160 +22,178 @@ import org.spongepowered.plugin.meta.version.ComparableVersion
 final class OreConfig @Inject()(config: Configuration) {
 
   // Sub-configs
-  lazy val root: Configuration = this.config
+  val root: Configuration = this.config
 
-  object app {
-    lazy val raw: Configuration               = root.get[Configuration]("application")
-    lazy val baseUrl: String                  = raw.get[String]("baseUrl")
-    lazy val dbDefaultTimeout: FiniteDuration = raw.get[FiniteDuration]("db.default-timeout")
-    lazy val uploadsDir: String               = raw.get[String]("uploadsDir")
+  object app extends ConfigCategory {
+    val raw: Configuration               = root.get[Configuration]("application")
+    val baseUrl: String                  = raw.get[String]("baseUrl")
+    val dbDefaultTimeout: FiniteDuration = raw.get[FiniteDuration]("db.default-timeout")
+    val uploadsDir: String               = raw.get[String]("uploadsDir")
 
-    lazy val trustedUrlHosts: Seq[String] = raw.get[Seq[String]]("trustedUrlHosts")
+    val trustedUrlHosts: Seq[String] = raw.get[Seq[String]]("trustedUrlHosts")
 
-    object fakeUser {
-      lazy val raw: Configuration    = app.raw.get[Configuration]("fakeUser")
-      lazy val enabled: Boolean      = raw.get[Boolean]("enabled")
-      lazy val id: ObjectReference   = raw.get[ObjectReference]("id")
-      lazy val name: Option[String]  = raw.getOptional[String]("name")
-      lazy val username: String      = raw.get[String]("username")
-      lazy val email: Option[String] = raw.getOptional[String]("email")
+    object fakeUser extends ConfigCategory {
+      val raw: Configuration    = app.raw.get[Configuration]("fakeUser")
+      val enabled: Boolean      = raw.get[Boolean]("enabled")
+      val id: ObjectReference   = raw.get[ObjectReference]("id")
+      val name: Option[String]  = raw.getOptional[String]("name")
+      val username: String      = raw.get[String]("username")
+      val email: Option[String] = raw.getOptional[String]("email")
     }
   }
 
-  object play {
-    lazy val raw: Configuration            = root.get[Configuration]("play")
-    lazy val sessionMaxAge: FiniteDuration = raw.get[FiniteDuration]("http.session.maxAge")
+  object play extends ConfigCategory {
+    val raw: Configuration            = root.get[Configuration]("play")
+    val sessionMaxAge: FiniteDuration = raw.get[FiniteDuration]("http.session.maxAge")
   }
 
-  object ore {
-    lazy val raw: Configuration = root.get[Configuration]("ore")
-    lazy val debug: Boolean     = raw.get[Boolean]("debug")
-    lazy val debugLevel: Int    = raw.get[Int]("debug-level")
+  object ore extends ConfigCategory {
+    val raw: Configuration = root.get[Configuration]("ore")
+    val debug: Boolean     = raw.get[Boolean]("debug")
+    val debugLevel: Int    = raw.get[Int]("debug-level")
 
-    object channels {
-      lazy val raw: Configuration  = ore.raw.get[Configuration]("channels")
-      lazy val maxNameLen: Int     = raw.get[Int]("max-name-len")
-      lazy val nameRegex: String   = raw.get[String]("name-regex")
-      lazy val colorDefault: Int   = raw.get[Int]("color-default")
-      lazy val nameDefault: String = raw.get[String]("name-default")
+    object channels extends ConfigCategory {
+      val raw: Configuration  = ore.raw.get[Configuration]("channels")
+      val maxNameLen: Int     = raw.get[Int]("max-name-len")
+      val nameRegex: String   = raw.get[String]("name-regex")
+      val colorDefault: Int   = raw.get[Int]("color-default")
+      val nameDefault: String = raw.get[String]("name-default")
     }
 
-    object pages {
-      lazy val raw: Configuration  = ore.raw.get[Configuration]("pages")
-      lazy val homeName: String    = raw.get[String]("home.name")
-      lazy val homeMessage: String = raw.get[String]("home.message")
-      lazy val minLen: Int         = raw.get[Int]("min-len")
-      lazy val maxLen: Int         = raw.get[Int]("max-len")
-      lazy val pageMaxLen: Int     = raw.get[Int]("page.max-len")
+    object pages extends ConfigCategory {
+      val raw: Configuration  = ore.raw.get[Configuration]("pages")
+      val homeName: String    = raw.get[String]("home.name")
+      val homeMessage: String = raw.get[String]("home.message")
+      val minLen: Int         = raw.get[Int]("min-len")
+      val maxLen: Int         = raw.get[Int]("max-len")
+      val pageMaxLen: Int     = raw.get[Int]("page.max-len")
     }
 
-    object projects {
-      lazy val raw: Configuration            = ore.raw.get[Configuration]("projects")
-      lazy val maxNameLen: Int               = raw.get[Int]("max-name-len")
-      lazy val maxPages: Int                 = raw.get[Int]("max-pages")
-      lazy val maxChannels: Int              = raw.get[Int]("max-channels")
-      lazy val initLoad: Int                 = raw.get[Int]("init-load")
-      lazy val initVersionLoad: Int          = raw.get[Int]("init-version-load")
-      lazy val maxDescLen: Int               = raw.get[Int]("max-desc-len")
-      lazy val fileValidate: Boolean         = raw.get[Boolean]("file-validate")
-      lazy val staleAge: FiniteDuration      = raw.get[FiniteDuration]("staleAge")
-      lazy val checkInterval: FiniteDuration = raw.get[FiniteDuration]("check-interval")
-      lazy val draftExpire: FiniteDuration   = raw.getOptional[FiniteDuration]("draft-expire").getOrElse(1.day)
+    object projects extends ConfigCategory {
+      val raw: Configuration            = ore.raw.get[Configuration]("projects")
+      val maxNameLen: Int               = raw.get[Int]("max-name-len")
+      val maxPages: Int                 = raw.get[Int]("max-pages")
+      val maxChannels: Int              = raw.get[Int]("max-channels")
+      val initLoad: Int                 = raw.get[Int]("init-load")
+      val initVersionLoad: Int          = raw.get[Int]("init-version-load")
+      val maxDescLen: Int               = raw.get[Int]("max-desc-len")
+      val fileValidate: Boolean         = raw.get[Boolean]("file-validate")
+      val staleAge: FiniteDuration      = raw.get[FiniteDuration]("staleAge")
+      val checkInterval: FiniteDuration = raw.get[FiniteDuration]("check-interval")
+      val draftExpire: FiniteDuration   = raw.getOptional[FiniteDuration]("draft-expire").getOrElse(1.day)
     }
 
-    object users {
-      lazy val raw: Configuration   = ore.raw.get[Configuration]("users")
-      lazy val starsPerPage: Int    = raw.get[Int]("stars-per-page")
-      lazy val maxTaglineLen: Int   = raw.get[Int]("max-tagline-len")
-      lazy val authorPageSize: Long = raw.get[Long]("author-page-size")
-      lazy val projectPageSize: Int = raw.get[Int]("project-page-size")
+    object users extends ConfigCategory {
+      val raw: Configuration   = ore.raw.get[Configuration]("users")
+      val starsPerPage: Int    = raw.get[Int]("stars-per-page")
+      val maxTaglineLen: Int   = raw.get[Int]("max-tagline-len")
+      val authorPageSize: Long = raw.get[Long]("author-page-size")
+      val projectPageSize: Int = raw.get[Int]("project-page-size")
     }
 
-    object orgs {
-      lazy val raw: Configuration       = ore.raw.get[Configuration]("orgs")
-      lazy val enabled: Boolean         = raw.get[Boolean]("enabled")
-      lazy val dummyEmailDomain: String = raw.get[String]("dummyEmailDomain")
-      lazy val createLimit: Int         = raw.get[Int]("createLimit")
+    object orgs extends ConfigCategory {
+      val raw: Configuration       = ore.raw.get[Configuration]("orgs")
+      val enabled: Boolean         = raw.get[Boolean]("enabled")
+      val dummyEmailDomain: String = raw.get[String]("dummyEmailDomain")
+      val createLimit: Int         = raw.get[Int]("createLimit")
     }
 
-    object queue {
-      lazy val raw: Configuration            = ore.raw.get[Configuration]("queue")
-      lazy val maxReviewTime: FiniteDuration = raw.getOptional[FiniteDuration]("max-review-time").getOrElse(1.day)
-    }
-  }
-
-  object forums {
-    lazy val raw: Configuration        = root.get[Configuration]("discourse")
-    lazy val baseUrl: String           = raw.get[String]("baseUrl")
-    lazy val categoryDefault: Int      = raw.get[Int]("categoryDefault")
-    lazy val categoryDeleted: Int      = raw.get[Int]("categoryDeleted")
-    lazy val retryRate: FiniteDuration = raw.get[FiniteDuration]("retryRate")
-
-    object api {
-      lazy val raw: Configuration      = forums.raw.get[Configuration]("api")
-      lazy val enabled: Boolean        = raw.get[Boolean]("enabled")
-      lazy val key: String             = raw.get[String]("key")
-      lazy val admin: String           = raw.get[String]("admin")
-      lazy val timeout: FiniteDuration = raw.get[FiniteDuration]("timeout")
+    object queue extends ConfigCategory {
+      val raw: Configuration            = ore.raw.get[Configuration]("queue")
+      val maxReviewTime: FiniteDuration = raw.getOptional[FiniteDuration]("max-review-time").getOrElse(1.day)
     }
   }
 
-  object sponge {
-    lazy val raw: Configuration  = root.get[Configuration]("sponge")
-    lazy val logo: String        = raw.get[String]("logo")
-    lazy val icon: String        = raw.get[String]("icon")
-    lazy val service: String     = raw.getOptional[String]("service").getOrElse("unknown")
-    lazy val sponsors: Seq[Logo] = raw.get[Seq[Logo]]("sponsors")
-  }
+  object forums extends ConfigCategory {
+    val raw: Configuration        = root.get[Configuration]("discourse")
+    val baseUrl: String           = raw.get[String]("baseUrl")
+    val categoryDefault: Int      = raw.get[Int]("categoryDefault")
+    val categoryDeleted: Int      = raw.get[Int]("categoryDeleted")
+    val retryRate: FiniteDuration = raw.get[FiniteDuration]("retryRate")
 
-  object security {
-    lazy val raw: Configuration         = root.get[Configuration]("security")
-    lazy val secure: Boolean            = raw.get[Boolean]("secure")
-    lazy val requirePgp: Boolean        = raw.get[Boolean]("requirePgp")
-    lazy val keyChangeCooldown: Long    = raw.get[Long]("keyChangeCooldown")
-    lazy val unsafeDownloadMaxAge: Long = raw.get[Long]("unsafeDownload.maxAge")
-
-    object api {
-      lazy val raw: Configuration      = security.raw.get[Configuration]("api")
-      lazy val url: String             = raw.get[String]("url")
-      lazy val avatarUrl: String       = raw.get[String]("avatarUrl")
-      lazy val key: String             = raw.get[String]("key")
-      lazy val timeout: FiniteDuration = raw.get[FiniteDuration]("timeout")
-    }
-
-    object sso {
-      lazy val raw: Configuration      = security.raw.get[Configuration]("sso")
-      lazy val loginUrl: String        = raw.get[String]("loginUrl")
-      lazy val signupUrl: String       = raw.get[String]("signupUrl")
-      lazy val verifyUrl: String       = raw.get[String]("verifyUrl")
-      lazy val secret: String          = raw.get[String]("secret")
-      lazy val timeout: FiniteDuration = raw.get[FiniteDuration]("timeout")
-      lazy val apikey: String          = raw.get[String]("apikey")
+    object api extends ConfigCategory {
+      val raw: Configuration      = forums.raw.get[Configuration]("api")
+      val enabled: Boolean        = raw.get[Boolean]("enabled")
+      val key: String             = raw.get[String]("key")
+      val admin: String           = raw.get[String]("admin")
+      val timeout: FiniteDuration = raw.get[FiniteDuration]("timeout")
     }
   }
 
-  object mail {
-    lazy val raw: Configuration        = root.get[Configuration]("mail")
-    lazy val username: String          = raw.get[String]("username")
-    lazy val email: String             = raw.get[String]("email")
-    lazy val password: String          = raw.get[String]("password")
-    lazy val smtpHost: String          = raw.get[String]("smtp.host")
-    lazy val smtpPort: Int             = raw.get[Int]("smtp.port")
-    lazy val transportProtocol: String = raw.get[String]("transport.protocol")
-    lazy val interval: FiniteDuration  = raw.get[FiniteDuration]("interval")
-
-    lazy val properties: Map[String, String] = raw.get[Map[String, String]]("properties")
+  object sponge extends ConfigCategory {
+    val raw: Configuration  = root.get[Configuration]("sponge")
+    val logo: String        = raw.get[String]("logo")
+    val icon: String        = raw.get[String]("icon")
+    val service: String     = raw.getOptional[String]("service").getOrElse("unknown")
+    val sponsors: Seq[Logo] = raw.get[Seq[Logo]]("sponsors")
   }
+
+  object security extends ConfigCategory {
+    val raw: Configuration         = root.get[Configuration]("security")
+    val secure: Boolean            = raw.get[Boolean]("secure")
+    val requirePgp: Boolean        = raw.get[Boolean]("requirePgp")
+    val keyChangeCooldown: Long    = raw.get[Long]("keyChangeCooldown")
+    val unsafeDownloadMaxAge: Long = raw.get[Long]("unsafeDownload.maxAge")
+
+    object api extends ConfigCategory {
+      val raw: Configuration      = security.raw.get[Configuration]("api")
+      val url: String             = raw.get[String]("url")
+      val avatarUrl: String       = raw.get[String]("avatarUrl")
+      val key: String             = raw.get[String]("key")
+      val timeout: FiniteDuration = raw.get[FiniteDuration]("timeout")
+    }
+
+    object sso extends ConfigCategory {
+      val raw: Configuration      = security.raw.get[Configuration]("sso")
+      val loginUrl: String        = raw.get[String]("loginUrl")
+      val signupUrl: String       = raw.get[String]("signupUrl")
+      val verifyUrl: String       = raw.get[String]("verifyUrl")
+      val secret: String          = raw.get[String]("secret")
+      val timeout: FiniteDuration = raw.get[FiniteDuration]("timeout")
+      val apikey: String          = raw.get[String]("apikey")
+    }
+  }
+
+  object mail extends ConfigCategory {
+    val raw: Configuration        = root.get[Configuration]("mail")
+    val username: String          = raw.get[String]("username")
+    val email: String             = raw.get[String]("email")
+    val password: String          = raw.get[String]("password")
+    val smtpHost: String          = raw.get[String]("smtp.host")
+    val smtpPort: Int             = raw.get[Int]("smtp.port")
+    val transportProtocol: String = raw.get[String]("transport.protocol")
+    val interval: FiniteDuration  = raw.get[FiniteDuration]("interval")
+
+    val properties: Map[String, String] = raw.get[Map[String, String]]("properties")
+  }
+
+  app.load()
+  app.fakeUser.load()
+  play.load()
+  ore.load()
+  ore.channels.load()
+  ore.pages.load()
+  ore.projects.load()
+  ore.users.load()
+  ore.orgs.load()
+  ore.queue.load()
+  forums.load()
+  forums.api.load()
+  sponge.load()
+  security.load()
+  security.api.load()
+  security.sso.load()
+  mail.load()
 
   /**
     * The default color used for Channels.
     */
-  lazy val defaultChannelColor: Color = Channel.Colors(ore.channels.colorDefault)
+  val defaultChannelColor: Color = Channel.Colors(ore.channels.colorDefault)
 
   /**
     * The default name used for Channels.
     */
-  lazy val defaultChannelName: String = ore.channels.nameDefault
+  val defaultChannelName: String = ore.channels.nameDefault
 
   /**
     * Returns true if the specified name is a valid Project name.
@@ -224,6 +242,10 @@ final class OreConfig @Inject()(config: Configuration) {
     if (!isDebug)
       throw new UnsupportedOperationException("this function is supported in debug mode only")
 
+}
+
+trait ConfigCategory {
+  def load(): Unit = ()
 }
 
 case class Logo(name: String, image: String, link: String)
