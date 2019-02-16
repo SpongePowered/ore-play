0277dbb3ec878e711ac9c51658368ca526e0bec2
Felix Kleinekathöfer <felixoiDev@gmail.com>
2019-02-16 17:00:20 +0100

    Rebase on master

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -13,7 +13,11 @@ var PLATFORM = null;
 $(function() {
     $('.select-sort').on('change', function() {
         var sort = $(this).find('option:selected').val();
-        go(jsRoutes.controllers.Application.showHome(CATEGORY_STRING, QUERY_STRING, sort, null, PLATFORM_CATEGORY, CATEGORY_STRING).absoluteURL);
+        go(jsRoutes.controllers.Application.showHome(CATEGORY_STRING, QUERY_STRING, sort, null, PLATFORM_CATEGORY, CATEGORY_STRING, ORDER_WITH_RELEVANCE).absoluteURL);
+    });
+
+    $('#relevanceBox').on('change', function() {
+        go(jsRoutes.controllers.Application.showHome(CATEGORY_STRING, QUERY_STRING, SORT_STRING, null, PLATFORM_CATEGORY, CATEGORY_STRING, this.checked).absoluteURL);
     });
 
     var projectSearch = $('.project-search');
@@ -26,6 +30,6 @@ $(function() {
 
     projectSearch.find('.btn').click(function() {
         var query = $(this).closest('.input-group').find('input').val();
-        go(jsRoutes.controllers.Application.showHome(CATEGORY_STRING, query, SORT_STRING, null, PLATFORM_CATEGORY, CATEGORY_STRING).absoluteURL);
+        go(jsRoutes.controllers.Application.showHome(CATEGORY_STRING, query, SORT_STRING, null, PLATFORM_CATEGORY, CATEGORY_STRING, ORDER_WITH_RELEVANCE).absoluteURL);
     });
 });
