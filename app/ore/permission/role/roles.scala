45a0090dcbef0d4ac5e473493eaf37f75272032d
Katrix <katrix97@hotmail.com>
2019-04-03 19:55:05 +0200

    Check for permissions to edit keys

--- app/ore/permission/role/roles.scala
+++ app/ore/permission/role/roles.scala
@@ -109,7 +109,13 @@ object Role extends StringEnum[Role] {
         "Project_Owner",
         19,
         RoleCategory.Project,
-        Perm(Perm.IsProjectOwner, Perm.DeleteProject, Perm.DeleteVersion, ProjectDeveloper.permissions),
+        Perm(
+          Perm.IsProjectOwner,
+          Perm.EditApiKeys,
+          Perm.DeleteProject,
+          Perm.DeleteVersion,
+          ProjectDeveloper.permissions
+        ),
         "Owner",
         Transparent,
         isAssignable = false
@@ -152,6 +158,7 @@ object Role extends StringEnum[Role] {
         25,
         RoleCategory.Organization,
         Perm(
+          Perm.EditApiKeys,
           Perm.ManageProjectMembers,
           Perm.EditOwnUserSettings,
           Perm.DeleteProject,
--- conf/evolutions/default/115.sql
+++ conf/evolutions/default/115.sql
@@ -1,7 +1,7 @@
 # --- !Ups
 
 ALTER TABLE roles
-  ADD COLUMN permission BIT(64) NOT NULL DEFAULT B'0'::BIT(64);
+    ADD COLUMN permission BIT(64) NOT NULL DEFAULT B'0'::BIT(64);
 UPDATE roles r
 SET permission = (-1)::BIT(64)
 WHERE r.name = 'Ore_Admin';
@@ -16,8 +16,8 @@ SET permission = (1::BIT(64) << 32) | (1::BIT(64) << 35)
 WHERE r.name = 'Web_Dev';
 
 UPDATE roles r
-SET permission = (1::BIT(64) << 4) | (1::BIT(64) << 5) | (1::BIT(64) << 6) | (1::BIT(64) << 10) | (1::BIT(64) << 14) |
-                 (1::BIT(64) << 12) | (1::BIT(64) << 13) | (1::BIT(64) << 15) | (1::BIT(64) << 9)
+SET permission = (1::BIT(64) << 2) | (1::BIT(64) << 4) | (1::BIT(64) << 5) | (1::BIT(64) << 6) | (1::BIT(64) << 10) |
+                 (1::BIT(64) << 14) | (1::BIT(64) << 12) | (1::BIT(64) << 13) | (1::BIT(64) << 15) | (1::BIT(64) << 9)
 WHERE r.name = 'Project_Owner';
 UPDATE roles r
 SET permission = (1::BIT(64) << 12) | (1::BIT(64) << 13) | (1::BIT(64) << 15) | (1::BIT(64) << 9)
@@ -27,15 +27,15 @@ SET permission = (1::BIT(64) << 9)
 WHERE r.name = 'Project_Editor';
 
 UPDATE roles r
-SET permission = (1::BIT(64) << 6) | (1::BIT(64) << 5) | (1::BIT(64) << 1) | (1::BIT(64) << 10) | (1::BIT(64) << 14) |
-                 (1::BIT(64) << 8) | (1::BIT(64) << 4) | (1::BIT(64) << 12) | (1::BIT(64) << 13) | (1::BIT(64) << 15) |
-                 (1::BIT(64) << 9) | (1::BIT(64) << 21)
+SET permission = (1::BIT(64) << 2) | (1::BIT(64) << 6) | (1::BIT(64) << 5) | (1::BIT(64) << 1) | (1::BIT(64) << 10) |
+                 (1::BIT(64) << 14) | (1::BIT(64) << 8) | (1::BIT(64) << 4) | (1::BIT(64) << 12) | (1::BIT(64) << 13) |
+                 (1::BIT(64) << 15) | (1::BIT(64) << 9) | (1::BIT(64) << 21)
 WHERE r.name = 'Organization_Owner'
    OR r.name = 'Organization';
 UPDATE roles r
-SET permission = (1::BIT(64) << 5) | (1::BIT(64) << 1) | (1::BIT(64) << 10) | (1::BIT(64) << 14) | (1::BIT(64) << 8) |
-                 (1::BIT(64) << 4) | (1::BIT(64) << 12) | (1::BIT(64) << 13) | (1::BIT(64) << 15) | (1::BIT(64) << 9) |
-                 (1::BIT(64) << 21)
+SET permission = (1::BIT(64) << 2) | (1::BIT(64) << 5) | (1::BIT(64) << 1) | (1::BIT(64) << 10) | (1::BIT(64) << 14) |
+                 (1::BIT(64) << 8) | (1::BIT(64) << 4) | (1::BIT(64) << 12) | (1::BIT(64) << 13) | (1::BIT(64) << 15) |
+                 (1::BIT(64) << 9) | (1::BIT(64) << 21)
 WHERE r.name = 'Organization_Admin';
 UPDATE roles r
 SET permission = (1::BIT(64) << 8) | (1::BIT(64) << 4) | (1::BIT(64) << 12) | (1::BIT(64) << 13) | (1::BIT(64) << 15) |
@@ -55,31 +55,31 @@ DROP VIEW organization_trust;
 CREATE OR REPLACE VIEW global_trust AS
 SELECT gr.user_id, coalesce(bit_or(r.permission), B'0'::bit(64)) AS permission
 FROM user_global_roles gr
-       JOIN roles r ON gr.role_id = r.id
+         JOIN roles r ON gr.role_id = r.id
 GROUP BY gr.user_id;
 
 CREATE OR REPLACE VIEW project_trust AS
 SELECT pm.project_id, pm.user_id, coalesce(bit_or(r.permission), B'0'::bit(64)) AS permission
 FROM project_members pm
-       JOIN user_project_roles rp ON pm.project_id = rp.project_id AND pm.user_id = rp.user_id AND rp.is_accepted
-       JOIN roles r ON rp.role_type = r.name
+         JOIN user_project_roles rp ON pm.project_id = rp.project_id AND pm.user_id = rp.user_id AND rp.is_accepted
+         JOIN roles r ON rp.role_type = r.name
 GROUP BY pm.project_id, pm.user_id;
 
 CREATE OR REPLACE VIEW organization_trust AS
 SELECT om.organization_id, om.user_id, coalesce(bit_or(r.permission), B'0'::bit(64)) AS permission
 FROM organization_members om
-       JOIN user_organization_roles ro
-            ON om.organization_id = ro.organization_id AND om.user_id = ro.user_id AND ro.is_accepted
-       JOIN roles r ON ro.role_type = r.name
+         JOIN user_organization_roles ro
+              ON om.organization_id = ro.organization_id AND om.user_id = ro.user_id AND ro.is_accepted
+         JOIN roles r ON ro.role_type = r.name
 GROUP BY om.organization_id, om.user_id;
 
 ALTER TABLE roles
-  DROP COLUMN trust;
+    DROP COLUMN trust;
 
 # --- !Downs
 
 ALTER TABLE roles
-  ADD COLUMN trust INTEGER NOT NULL DEFAULT 0;
+    ADD COLUMN trust INTEGER NOT NULL DEFAULT 0;
 
 UPDATE roles r
 SET trust = 5
@@ -119,23 +119,23 @@ DROP VIEW organization_trust;
 CREATE OR REPLACE VIEW global_trust AS
 SELECT gr.user_id, coalesce(max(r.trust), 0) AS trust
 FROM user_global_roles gr
-       JOIN roles r ON gr.role_id = r.id
+         JOIN roles r ON gr.role_id = r.id
 GROUP BY gr.user_id;
 
 CREATE OR REPLACE VIEW project_trust AS
 SELECT pm.project_id, pm.user_id, coalesce(max(r.trust), 0) AS trust
 FROM project_members pm
-       JOIN user_project_roles rp ON pm.project_id = rp.project_id AND pm.user_id = rp.user_id
-       JOIN roles r ON rp.role_type = r.name
+         JOIN user_project_roles rp ON pm.project_id = rp.project_id AND pm.user_id = rp.user_id
+         JOIN roles r ON rp.role_type = r.name
 GROUP BY pm.project_id, pm.user_id;
 
 CREATE OR REPLACE VIEW organization_trust AS
 SELECT om.organization_id, om.user_id, coalesce(max(r.trust), 0) AS trust
 FROM organization_members om
-       JOIN user_organization_roles ro
-            ON om.organization_id = ro.organization_id AND om.user_id = ro.user_id
-       JOIN roles r ON ro.role_type = r.name
+         JOIN user_organization_roles ro
+              ON om.organization_id = ro.organization_id AND om.user_id = ro.user_id
+         JOIN roles r ON ro.role_type = r.name
 GROUP BY om.organization_id, om.user_id;
 
 ALTER TABLE roles
-  DROP COLUMN permission;
+    DROP COLUMN permission;
