4ecdf64f78ee369cb2615a9cbd8b622fdbc83251
Felix Kleinekathöfer <felixoiDev@gmail.com>
2019-04-29 17:18:39 +0200

    Fix evolution

--- ore/conf/evolutions/default/119.sql
+++ ore/conf/evolutions/default/119.sql
@@ -2,4 +2,5 @@
 ALTER TABLE notifications ALTER COLUMN origin_id DROP NOT NULL;
 
 # --- !Downs
+DELETE FROM notifications WHERE origin_id IS NULL;
 ALTER TABLE notifications ALTER COLUMN origin_id SET NOT NULL;
