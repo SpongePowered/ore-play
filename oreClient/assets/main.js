8b0d9e3d8646bbea3051dbe5fca86191d9c7656d
Katrix <katrix97@hotmail.com>
2019-05-28 21:47:12 +0200

    IT WORKS!!!

--- oreClient/assets/main.js
+++ oreClient/assets/main.js
@@ -1,6 +1,6 @@
 const Vue = require('vue');
 const root = require('./Test.vue').default;
-const app = new Vue({
+const app = new Vue.default({
     el: '#app',
     render: createElement => createElement(root),
 });
