8476fb89f0e6582c5265dbccc71d9ddb005912f6
Felix Kleinekathöfer <felixoiDev@gmail.com>
2019-10-14 11:05:32 +0200

    Load stats using api

--- ore/public/javascripts/projectDetail.js
+++ ore/public/javascripts/projectDetail.js
@@ -252,6 +252,10 @@ $(function() {
                 });
                 $(".promoted-list").html(html);
             }
-        })
+            $(".stats #view-count").html(response.stats.views);
+            $(".stats #star-count").html(response.stats.stars);
+            $(".stats #watcher-count").html(response.stats.watchers);
+            $(".stats #download-count").html(response.stats.downloads);
+        });
     }
 });
