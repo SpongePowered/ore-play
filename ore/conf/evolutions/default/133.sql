23674057ae01c3c5cb8c86395561cc1c963aff46
Katrix <katrix97@hotmail.com>
2019-12-13 19:30:19 +0100

    Fix evolution

--- ore/conf/evolutions/default/133.sql
+++ ore/conf/evolutions/default/133.sql
@@ -5,12 +5,12 @@ CREATE OR REPLACE FUNCTION websearch_to_tsquery_postfix(dictionary REGCONFIG, qu
     LANGUAGE plpgsql AS
 $$
 DECLARE
-    arr  TEXT[]  := regexp_split_to_array(query, '\s+');
-    last TEXT    := websearch_to_tsquery(dictionary, arr[array_length(arr, 1)])::TEXT;
-    init TSQUERY := websearch_to_tsquery(dictionary, regexp_replace(query, '\S+$', ''));
+    arr  TEXT[]  := regexp_split_to_array(query, '\s+');;
+    last TEXT    := websearch_to_tsquery(dictionary, arr[array_length(arr, 1)])::TEXT;;
+    init TSQUERY := websearch_to_tsquery(dictionary, regexp_replace(query, '\S+$', ''));;
 BEGIN
-    RETURN init && to_tsquery(last || ':*');
-END;
+    RETURN init && to_tsquery(last || ':*');;
+END;;
 $$;
 
 # --- !Downs
