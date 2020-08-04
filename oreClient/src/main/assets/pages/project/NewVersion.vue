b667aca17d27e067888fd951a019d161a862dac3
Felix Kleinekathöfer <felixoiDev@gmail.com>
2020-08-04 10:45:32 +0200

    Styling and upload fixes

--- oreClient/src/main/assets/pages/project/NewVersion.vue
+++ oreClient/src/main/assets/pages/project/NewVersion.vue
@@ -1,125 +1,132 @@
 <template>
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h3 class="panel-title">New project release</h3>
-    </div>
+  <div>
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h3 class="panel-title">New project release</h3>
+      </div>
 
-    <div class="panel-body">
-      <div class="row">
-        <div class="col-md-4"></div>
-        <div class="col-md-4">
-          <label class="btn btn-primary btn-lg btn-block">
-            <input
-              ref="fileInput"
-              type="file"
-              style="display: none;"
-              :disabled="processing"
-              accept=".jar, application/zip"
-              @change="processFileUpload"
-            />
-            <span v-if="!processing && !versionPreview">
+      <div class="panel-body">
+        <div class="row">
+          <div class="col-md-4"></div>
+          <div class="col-md-4">
+            <div class="mb-1 text-center" v-if="!processing && versionPreview">{{ this.$refs.fileInput.files[0].name }}</div>
+            <label class="btn btn-primary btn-block" :class="{'btn-lg': (!processing && !versionPreview)}">
+              <input
+                  ref="fileInput"
+                  type="file"
+                  style="display: none;"
+                  :disabled="processing"
+                  accept=".jar, application/zip"
+                  @change="processFileUpload"
+              />
+              <span v-if="!processing && !versionPreview">
               Select file
             </span>
-            <span v-else-if="!versionPreview"> Processing <font-awesome-icon :icon="['fas', 'spinner']" spin /> </span>
-            <span v-else>
+              <span v-else-if="!versionPreview"> Processing <font-awesome-icon :icon="['fas', 'spinner']" spin /> </span>
+              <span v-else>
               Upload a different file
             </span>
-          </label>
-        </div>
-        <div class="col-md-4"></div>
-      </div>
-    </div>
-
-    <div v-if="versionPreview" class="panel-heading">
-      <h3 class="panel-title">Version info</h3>
-    </div>
-
-    <div v-if="versionPreview" class="panel-body">
-      <div class="form-horizontal">
-        <div class="form-group">
-          <label class="col-sm-2 control-label">Name:</label>
-          <div class="col-sm-10">
-            <p class="form-control-static">{{ versionPreview.name }}</p>
+            </label>
           </div>
-        </div>
-        <div class="form-group">
-          <label for="createForumPost" class="col-sm-2 control-label">Create forum post:</label>
-          <div class="col-sm-10">
-            <input id="createForumPost" v-model="createForumPost" type="checkbox" class="form-control-static" />
-          </div>
-        </div>
-        <div class="form-group">
-          <label class="col-sm-2 control-label">File name:</label>
-          <div class="col-sm-10">
-            <p class="form-control-static">{{ versionPreview.file_info.name }}</p>
-          </div>
-        </div>
-        <div class="form-group">
-          <label class="col-sm-2 control-label">File size:</label>
-          <div class="col-sm-10">
-            <p class="form-control-static">{{ formatBytes(versionPreview.file_info.size_bytes) }}</p>
-          </div>
-        </div>
-        <div class="form-group">
-          <label class="col-sm-2 control-label">File hash:</label>
-          <div class="col-sm-10">
-            <p class="form-control-static">{{ versionPreview.file_info.md5_hash }}</p>
-          </div>
-        </div>
-        <div class="form-group">
-          <label class="col-sm-2 control-label">Mixin:</label>
-          <div class="col-sm-10">
-            <p class="form-control-static">{{ versionPreview.tags.mixin }}</p>
-          </div>
-        </div>
-        <div class="form-group">
-          <label for="stabilitySelect" class="col-sm-2 control-label">Stability:</label>
-          <div class="col-sm-10">
-            <select id="stabilitySelect" v-model="selectedStability" class="form-control">
-              <option v-for="stability in stabilities" :key="stability.id" :value="stability.id">
-                {{ stability.title }}
-              </option>
-            </select>
-          </div>
-        </div>
-        <div class="form-group">
-          <label for="releaseTypeSelect" class="col-sm-2 control-label">Release type:</label>
-          <div class="col-sm-10">
-            <select id="releaseTypeSelect" v-model="selectedReleaseType" class="form-control">
-              <option :value="null">None</option>
-              <option v-for="releaseType in releaseTypes" :key="releaseType.id" :value="releaseType.id">
-                {{ releaseType.title }}
-              </option>
-            </select>
-          </div>
-        </div>
-        <div class="form-group">
-          <label class="col-sm-2 control-label">Platforms:</label>
-          <div class="col-sm-10">
-            <ul class="list-unstyled">
-              <li
-                v-for="platform in versionPreview.tags.platforms"
-                :key="platform.platform + platform.platform_version"
-              >
-                {{ platforms.fromId(platform.platform).shortName }} ({{ platform.platform }}):
-                {{ platform.platform_version }}
-              </li>
-            </ul>
-          </div>
+          <div class="col-md-4"></div>
         </div>
       </div>
     </div>
 
-    <div v-if="versionPreview" class="panel-heading">
-      <h3 class="panel-title">Version description</h3>
-    </div>
-    <div v-if="versionPreview" class="panel-body">
-      <editor :enabled="true" :raw="description" @change="(data) => (description = data)" @saved="(data) => (description = data)"></editor>
+    <div class="panel panel-default" v-if="versionPreview">
+      <div v-if="versionPreview" class="panel-heading">
+        <h3 class="panel-title">Version info</h3>
+      </div>
 
-      <button class="btn btn-success" @click="publishVersion">
-        <span v-if="!uploadProcessing">Publish version</span>
-        <span v-else> Processing <font-awesome-icon :icon="['fas', 'spinner']" spin /> </span>
-      </button>
+      <div v-if="versionPreview" class="panel-body">
+        <div class="form-horizontal">
+          <div class="form-group">
+            <label class="col-sm-2 control-label">Name:</label>
+            <div class="col-sm-10">
+              <p class="form-control-static">{{ versionPreview.name }}</p>
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="createForumPost" class="col-sm-2 control-label">Create forum post:</label>
+            <div class="col-sm-10">
+              <input id="createForumPost" v-model="createForumPost" type="checkbox" class="form-control-static" />
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="col-sm-2 control-label">File name:</label>
+            <div class="col-sm-10">
+              <p class="form-control-static">{{ versionPreview.file_info.name }}</p>
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="col-sm-2 control-label">File size:</label>
+            <div class="col-sm-10">
+              <p class="form-control-static">{{ formatBytes(versionPreview.file_info.size_bytes) }}</p>
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="col-sm-2 control-label">File hash:</label>
+            <div class="col-sm-10">
+              <p class="form-control-static">{{ versionPreview.file_info.md5_hash }}</p>
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="col-sm-2 control-label">Mixin:</label>
+            <div class="col-sm-10">
+              <p class="form-control-static">{{ versionPreview.tags.mixin }}</p>
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="stabilitySelect" class="col-sm-2 control-label">Stability:</label>
+            <div class="col-sm-10">
+              <select id="stabilitySelect" v-model="selectedStability" class="form-control">
+                <option v-for="stability in stabilities" :key="stability.id" :value="stability.id">
+                  {{ stability.title }}
+                </option>
+              </select>
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="releaseTypeSelect" class="col-sm-2 control-label">Release type:</label>
+            <div class="col-sm-10">
+              <select id="releaseTypeSelect" v-model="selectedReleaseType" class="form-control">
+                <option :value="null">None</option>
+                <option v-for="releaseType in releaseTypes" :key="releaseType.id" :value="releaseType.id">
+                  {{ releaseType.title }}
+                </option>
+              </select>
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="col-sm-2 control-label">Platforms:</label>
+            <div class="col-sm-10">
+              <ul class="list-unstyled">
+                <li
+                    v-for="platform in versionPreview.tags.platforms"
+                    :key="platform.platform + platform.platform_version"
+                >
+                  {{ platforms.fromId(platform.platform).shortName }} ({{ platform.platform }}):
+                  {{ platform.platform_version }}
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="panel panel-default" v-if="versionPreview">
+      <div v-if="versionPreview" class="panel-heading">
+        <h3 class="panel-title">Version description</h3>
+      </div>
+      <div v-if="versionPreview" class="panel-body">
+        <editor :enabled="true" :raw="description" @change="(data) => (description = data)" @saved="(data) => (description = data)"></editor>
+
+        <button class="btn btn-success" @click="publishVersion">
+          <span v-if="!uploadProcessing">Publish version</span>
+          <span v-else> Processing <font-awesome-icon :icon="['fas', 'spinner']" spin /> </span>
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -171,6 +178,12 @@ export default {
       return parseFloat((bytes / k ** i).toFixed(dm)) + ' ' + sizes[i]
     },
     async processFileUpload() {
+      if(this.$refs.fileInput.files.length !== 1 && this.versionPreview) {
+        this.processing = false
+        this.versionPreview = false
+        return
+      }
+
       this.processing = true
       this.$store.commit('dismissAllAlerts')
 
